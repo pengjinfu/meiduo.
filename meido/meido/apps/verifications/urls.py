c26e27805c1e56c4c19851f603d7e81ae9ea16ea
pengjinfu <pengjinfupy@163.com>
2018-09-18 15:25:19 +0800

    接口的说明与短信验证码的后端实现

--- meido/meido/apps/verifications/urls.py
+++ meido/meido/apps/verifications/urls.py
@@ -1,14 +1,7 @@
-#!/usr/bin/python3
-# -*- coding:utf-8 -*-
-# Author:Dreamer
-# Time:2018.9.1
-
-
 from django.conf.urls import url
-
-from meido.apps.verifications import views
+from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url('^sms_codes/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view()),
-
+    url(r'^sms_codes/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view()),
 ]
